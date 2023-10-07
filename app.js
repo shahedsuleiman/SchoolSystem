7ffb1a95f3b13833f1aa77cb23294258c10d9e39
shahedsuleiman <shahedsuleiman2001@gmail.com>
2023-10-07 09:37:26 +0300

    avatar card

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
+class Student { 
+    constructor(fullName, db, gender, major, imageURL, phoneNumber) {
+        this.fullName = fullName;
+        this.db = db;
+        this.gender = gender;
+        this.major = major;
+        this.imageUrl = imageURL;
+        this.phoneNumber = phoneNumber;
+    } 
+}
+
+let students = JSON.parse(localStorage.getItem('students')) || [];
+let cards = document.getElementById('cardContainer');
+    
 function getInput(event){
     event.preventDefault();
 
@@ -5,45 +19,53 @@ function getInput(event){
     let bd = document.getElementById("bd").value;
     let gender = getGender();
     let tel = document.getElementById("phone").value;
-    let grade = document.getElementById("grade");
-    let check = grade.options[grade.selectedIndex].text;
-    const phonepatt= /^(?:\+962|0)(7[789]|8[0123456789])[0-9]{7}$/;
-    let table = document.getElementById("table");
-    let tableBody = document.getElementById("table-body");
-    let newRow = tableBody.insertRow(tableBody.rows.length)
+    let imageUrl = document.getElementById("imageUrl").value;
+    let major = document.getElementById("major").value;
     
-  
-    let cel1 = newRow.insertCell(0);
-	let cel2 = newRow.insertCell(1);
-	let cel3 = newRow.insertCell(2);
-	let cel4 = newRow.insertCell(3);
-	let cel5 = newRow.insertCell(4);
-    
-    if(phonepatt.test(tel) == false)
-    {
-        document.getElementById("phone_alert").innerHTML = "Numbers only Allowed";
-        document.getElementById("phone_alert").style.color = "red";
+    const phonepatt = /^(?:\+962|0)(7[789]|8[0123456789])[0-9]{7}$/;
+
+    if (!phonepatt.test(tel)) {
+        alert("Invalid phone number");
+        return;
     }
-	cel1.innerHTML = name;
-	cel2.innerHTML = bd;
-	cel3.innerHTML = gender;
-	cel4.innerHTML = tel;
-	cel5.innerHTML = check;
-			
+
+    let std = new Student(name, bd, gender, major, imageUrl, tel);
+
+    students.push(std);
+    localStorage.setItem('students', JSON.stringify(students));
+
+    displayStudents();
+    
 }
-function getGender()
-{
+
+function displayStudents() {
+    cards.innerHTML = ''; 
+
+    students.forEach(function(std) {
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.innerHTML = `
+            <img  style="width:250px;height:200px; " src="${std.imageUrl}" alt="${std.fullName}">
+            <h2>${std.fullName}</h2>
+            <p>Major: ${std.major}</p>
+            <p>Phone Number: ${std.phoneNumber}</p>
+        `;
+        cards.appendChild(card);
+    });
+
+}
+
+function getGender() {
     let f = document.getElementById("female").checked;
     let m = document.getElementById("male").checked;
 
-
-    if( f )
-    {
+    if (f) {
         return "Female";
-    }else if(m)
-    {
-       return "Male";
+    } else if (m) {
+        return "Male";
     }
-    return "";			
+    return "";
 }
+
+
 // document.getElementById("table").addEventListener("submit", getInput);
